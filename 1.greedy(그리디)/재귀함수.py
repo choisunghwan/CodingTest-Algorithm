605a12877fd816ad47c1f3e2d900d3f2d98d345e
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-06-30 20:42:09 +0900

    재귀함수

---
+++ 1.greedy(그리디)/재귀함수.py
@@ -0,0 +1,9 @@
+def recursive_function():
+    print('재귀함수를 호출합니다.')
+    recursive_function()
+
+recursive_function()
+
+
+# 재귀함수(Recursive Function) : 자기 자신을 다시 호출하는 함수를 의미한다.
+# 재귀함수를 호출합니다' 라는 문자열을 무한히 출력하다가 최대 깊이 초과 메시지가 출력됨.
